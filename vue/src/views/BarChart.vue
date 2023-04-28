6c049ebe739a19b6ec955043c6551e53d57c5bcd
Sophie Dong <sophiedong2007@gmail.com>
2023-04-28 10:14:04 -0400

    Revert "x"
    This reverts commit 1abb29a7c44a45c465dc0c01012f778c74ea6428.

--- vue/src/views/BarChart.vue
+++ vue/src/views/BarChart.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="chart">
-    <RouterLink to="/">Main</RouterLink>
+    <RouterLink to="/">Back to Main</RouterLink>
     <Doughnut v-if="loaded" :data="chartData" :options="chartOptions" />
   </div>
-  <BarChart />
 </template>
 
 <script>
@@ -19,7 +18,7 @@ export default {
     return {
       loaded: false,
       chartData: {
-        labels: ['<18', '18-24', '25-44', '45-64', '65+'],
+        labels: ['<18', '18-24', '25-44', '45-64', '65"'],
         datasets: [{ data: [] }]
       },
       chartOptions: {
@@ -31,18 +30,14 @@ export default {
   },
   async mounted() {
     try {
-      const res = await fetch('https://data.cityofnewyork.us/resource/uip8-fykc.json')
-      let data = await res.json()
-      const lessEighteen = datasets.filter((crime) => crime.age_group === '<18')
-      this.chartData.datasets[0].data.push(lessEighteen.length)
-      const oneTwo = data.filter((crime) => crime.age_group === '18-24')
-      this.chartData.datasets[0].data.push(oneTwo.length)
-      const twoFour = data.filter((crime) => crime.age_group === '25-44')
-      this.chartData.datasets[0].data.push(twoFour.length)
-      const fourSix = data.filter((crime) => crime.age_group === '45-64')
-      this.chartData.datasets[0].data.push(fourSix.length)
-      const sixtyFive = data.filter((crime) => crime.age_group === '65+')
-      this.chartData.datasets[0].data.push(sixtyFive.length)
+      const res = await fetch('https://data.cityofnewyork.us/resource/vfnx-vebw.json')
+      const squirrelData = await res.json()
+      const gray = squirrelData.filter((squirrel) => squirrel.primary_fur_color === 'Gray')
+      this.chartData.datasets[0].data.push(gray.length)
+      const cinnamon = squirrelData.filter((squirrel) => squirrel.primary_fur_color === 'Cinnamon')
+      this.chartData.datasets[0].data.push(cinnamon.length)
+      const black = squirrelData.filter((squirrel) => squirrel.primary_fur_color === 'Black')
+      this.chartData.datasets[0].data.push(black.length)
       this.loaded = true
     } catch (e) {
       console.error(e)
