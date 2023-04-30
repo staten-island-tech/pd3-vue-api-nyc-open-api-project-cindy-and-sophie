bcca0425f95713b02a0641b52deca1d186cc7733
Sophie Dong <sophiedong2007@gmail.com>
2023-04-30 19:48:14 -0400

    Update index.js

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -22,7 +22,7 @@ const router = createRouter({
     {
       path: '/PieChart',
       name: 'PieChart',
-      component: () => import('../views/PieChart.vue')
+      component: () => import('../views/Piechart.vue')
     }
   ]
 })
